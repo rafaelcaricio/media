0ae930354214e61e79c3d8ca3cd0d51e1868a329
Rafael Caricio <rafael@caricio.com>
2023-05-16 10:35:19 +0200

    Use GStreamer also on ARM Linux

--- backends/auto/lib.rs
+++ backends/auto/lib.rs
@@ -4,7 +4,7 @@
         any(target_arch = "arm", target_arch = "aarch64")
     ),
     target_arch = "x86_64",
-    all(target_os = "macos", any(target_arch = "x86", target_arch = "aarch64"))
+    all(any(target_os = "macos", target_os = "linux"), target_arch = "aarch64")
 ))]
 mod platform {
     extern crate servo_media_gstreamer;
@@ -17,7 +17,7 @@ mod platform {
         any(target_arch = "arm", target_arch = "aarch64")
     ),
     target_arch = "x86_64",
-    all(target_os = "macos", any(target_arch = "x86", target_arch = "aarch64"))
+    all(any(target_os = "macos", target_os = "linux"), target_arch = "aarch64")
 )))]
 mod platform {
     extern crate servo_media_dummy;
